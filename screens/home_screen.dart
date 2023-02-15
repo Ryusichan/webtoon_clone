70c1b9682ad504c7ec41f8213532c901358089e4
Ryusichan <Ryusichan@github.com>
2023-02-15 17:25:03 +0900

    일반적인 api설정한걸 받아오는 방법 1. initstate로 초기화 2. await, async 로 데이터받아오는 상황정리 3. setstate로 다시 초기화

--- screens/home_screen.dart
+++ screens/home_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:webappdemo/models/webtoon.dart';
+import 'package:webappdemo/server/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonDataBinding> webtoon = [];
+  bool isLoading = true;
+
+  void waitforWebtoon() async {
+    webtoon = await ApiService.getTodayWebtoon();
+    isLoading = false;
+    // 화면을 새로고침하기 위해서 setstate사용
+    setState(() {});
+  }
+
+// initstate에 데이터를 받아오는 함수를 호출
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    waitforWebtoon();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoon);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
