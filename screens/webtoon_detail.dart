52de55b53f31f53a0009454c0fbd684015303b28
Ryusichan <Ryusichan@github.com>
2023-02-16 18:31:41 +0900

    webtoondetail 레이아웃 수정

--- screens/webtoon_detail.dart
+++ screens/webtoon_detail.dart
@@ -8,19 +8,47 @@ class WebtoonDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 2,
-          title: const Text(
-            '아파트지금',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-            ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 2,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
           ),
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.blue,
-        ));
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: const Offset(10, 10),
+                        color: Colors.grey.withOpacity(0.3),
+                      )
+                    ]),
+                child: Image.network(thumb),
+              ),
+            ],
+          ),
+          Text(title)
+        ],
+      ),
+    );
   }
 }
