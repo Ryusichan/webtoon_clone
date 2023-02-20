b6f4c720535d4b4321c2f472b4c8c72daf875705
Ryusichan <Ryusichan@github.com>
2023-02-20 17:18:13 +0900

    api 등록내용을 불러오기 초기화 하고 싶은 property가 constructor에서 불가능한 경우 staful처리 함수로 initstate 초기화해줌

--- screens/webtoon_detail.dart
+++ screens/webtoon_detail.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:webappdemo/models/webtoonDetail_model.dart';
+import 'package:webappdemo/models/webtoonEpisode_model.dart';
+import 'package:webappdemo/server/api_service.dart';
 
-class WebtoonDetail extends StatelessWidget {
+class WebtoonDetail extends StatefulWidget {
   final String thumb, title, id;
+
   const WebtoonDetail(
       {super.key, required this.thumb, required this.title, required this.id});
+
+  @override
+  State<WebtoonDetail> createState() => _WebtoonDetailState();
+}
+
+class _WebtoonDetailState extends State<WebtoonDetail> {
+  // 초기화하고 싶은 property가 있지만 constructor에서 불가능한 경우 late를 붙여서 함수로 initState에서 초기화진행
+  // initState는 build보다 먼저 호출됨
+
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // stateful로 바꿔줌으로써 Future를 안전하게 초기화 진행가능
+    webtoon = ApiService.getDetailWebtoon(widget.id);
+    episodes = ApiService.getEpisodeWebtoon(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +37,7 @@ class WebtoonDetail extends StatelessWidget {
         centerTitle: true,
         elevation: 2,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.w500,
@@ -31,7 +55,8 @@ class WebtoonDetail extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                // widget은 WebtoonDetail를 의미한다 많은 staful방식으로 변경되었기때문에 widget을통해 접근이 가능하다
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -44,12 +69,12 @@ class WebtoonDetail extends StatelessWidget {
                           color: Colors.grey.withOpacity(0.3),
                         )
                       ]),
-                  child: Image.network(thumb),
+                  child: Image.network(widget.thumb),
                 ),
               ),
             ],
           ),
-          Text(title)
+          Text(widget.title)
         ],
       ),
     );
