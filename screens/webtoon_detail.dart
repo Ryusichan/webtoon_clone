23e6fc2a35815e95a2ef4189c5ef3d33218ed8b0
Ryusichan <Ryusichan@github.com>
2023-02-22 16:04:51 +0900

    좋아하는 웹툰에 등록하기 localstage와 비슷한 기능 모바일 등록 pakage설치후 이용해보기

--- screens/webtoon_detail.dart
+++ screens/webtoon_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webappdemo/models/webtoondetail_model.dart';
 import 'package:webappdemo/models/webtoonepisode_model.dart';
 import 'package:webappdemo/server/api_service.dart';
@@ -7,8 +8,12 @@ import 'package:webappdemo/widgets/episode_widget.dart';
 class WebtoonDetail extends StatefulWidget {
   final String thumb, title, id;
 
-  const WebtoonDetail(
-      {super.key, required this.thumb, required this.title, required this.id});
+  const WebtoonDetail({
+    super.key,
+    required this.thumb,
+    required this.title,
+    required this.id,
+  });
 
   @override
   State<WebtoonDetail> createState() => _WebtoonDetailState();
@@ -20,6 +25,32 @@ class _WebtoonDetailState extends State<WebtoonDetail> {
 
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  // 모바일사용자에게 등록해주는  pakage를 사용자의 클래스에 등록
+  late SharedPreferences prefs;
+  // state등록해준다
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    // 사용자의 connection에 instance를 등록해준다.
+    prefs = await SharedPreferences.getInstance();
+    // List<String>? string일수도있고 아닐수도 있다 최초에 등록한 list는 내용을 알수 없기때문
+    // 그래서 likedToons상태를 체크해준다
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      // 현재 대상 아이디에 좋아요를 누른적이있는지 확인여부
+      // widget의 대상을 추격하는건 현재는 state상태이고 WebtoonDetail의 id를 추격하기위해서
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      } else {}
+    } else {
+      // 사용자 최초등록시 상황정리
+      // likedToons이 최초에 null 아니라면 등록해준다 Future이기때문에 await를 붙여줌
+      await prefs.setStringList('likedToons', []);
+    }
+    print(isLiked);
+  }
 
   @override
   void initState() {
@@ -28,6 +59,27 @@ class _WebtoonDetailState extends State<WebtoonDetail> {
     // stateful로 바꿔줌으로써 Future를 안전하게 초기화 진행가능
     webtoon = ApiService.getDetailWebtoon(widget.id);
     episodes = ApiService.getEpisodeWebtoon(widget.id);
+
+    initPrefs();
+  }
+
+  onFavoriteTap() async {
+    // 'likedToons' 리스트 가져오기
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+
+      // 마지막에 최종결정된 liked를 prefs에 넣어줌
+      await prefs.setStringList('likedToons', likedToons);
+      // 누를때마다 state변경
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
   }
 
   @override
@@ -46,6 +98,13 @@ class _WebtoonDetailState extends State<WebtoonDetail> {
         ),
         backgroundColor: Colors.white,
         foregroundColor: Colors.blue,
+        actions: [
+          IconButton(
+              onPressed: onFavoriteTap,
+              icon: Icon(isLiked
+                  ? Icons.favorite_outlined
+                  : Icons.favorite_outline_outlined))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 40),
